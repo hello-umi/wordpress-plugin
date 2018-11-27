5305a4f606deb68f74b888b56768ce991496be69
Carlos Ruiz <carlosruiz@MacBook-Air-de-Carlos.local>
2018-11-27 14:47:14 +0100

    refactor code in process

--- index.php
+++ index.php
@@ -268,130 +268,101 @@ class Landbot {
 
 	?>
 
-	<div class="wrap">
-        <form id="landbot-admin-form" class="postbox">
-
-                <div class="form-group inside">
-                    <h1>
-		                Add Landbot
-                    </h1>
-
-	                <?php if ($not_ready): ?>
-                        <p>
-                            <?php _e('Make sure you have a Landbot account first, it\'s free! 👍', 'landbot'); ?>
-                            <?php _e('You can <a href="https://landbot.io" target="_blank">create an account here</a>.', 'landbot'); ?>
-                            <br>
-	                        <?php _e('Once the keys set and saved, if you do not see any option, please reload the page. Thank you, you rock 🤘', 'landbot'); ?>
-                        </p>
-                    <?php else: ?>
-		                <?php _e('Access your <a href="https://landbot.io" target="_blank">Landbot dashboard here</a>.', 'landbot'); ?>
-                    <?php endif; ?>
-
-                        <div class="content-section">
-                            <div>
-                                <label>1. Copy and paste your landbot token here*</label>
-                                <label>*You can find it under your Landbot > Share section</label>
-                            </div>
-                            <div class="authorization">
-                                <div>
-                                    TOKEN:
-                                </div>
-                                <div>
-                                  <input
-                                    class="regular-text" 
-                                    name="authorization"
-                                    id="authorization"
-                                    placeholder="Token 7beqb8161dcbw715018i1f26axa8901b94az563b"
-                                    type="text"
-                                  />
-                                </div>
-                            </div>
-                        </div>
-                        <div class="content-section">
-                            <div>
-                                <label>2. Display Format</label>
-                                <label>*The way Landbot is displayed</label>
-                            </div>
-                            <div>
-                                <div onclick="checkDisplayFormat('FULLPAGE')" class="square-display-format display-format-color">
-                                    <div>
-                                      <img src="<?php echo plugin_dir_url( __FILE__ ) . '/assets/img/full.png'; ?>" alt="fullpage"/>
-                                    </div>
-                                    <div>
-                                      FULLPAGE
-                                    </div>  
-                                </div>
-                                <div onclick="checkDisplayFormat('POPUP')" class="square-display-format display-format-color">
-                                    <div>
-                                      <img src="<?php echo plugin_dir_url( __FILE__ ) . '/assets/img/popup.png'; ?>" alt="popup"/>
-                                    </div>
-                                    <div>
-                                      POPUP
-                                    </div>
-                                </div>
-                                <div onclick="checkDisplayFormat('EMBED')" class="square-display-format display-format-color">
-                                    <div>
-                                      <img src="<?php echo plugin_dir_url( __FILE__ ) . '/assets/img/embed.png'; ?>" alt="embed"/>
-                                    </div>
-                                    <div>
-                                      EMBED
-                                    </div>
-                                </div>
-                                <div onclick="checkDisplayFormat('LIVE CHAT')" class="square-display-format display-format-color">
-                                    <div>
-                                      <img src="<?php echo plugin_dir_url( __FILE__ ) . '/assets/img/LIVECHAT.png'; ?>" alt="LIVE CHAT"/>
-                                    </div>
-                                    <div>
-                                      LIVE CHAT
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="content-section">
-                            <div>
-                                <label>3. More Options</label>
-                            </div>
-                            <div class="more-options">
-                                <div>
-                                    <div class="option-check">
-                                        <div>
-                                          Hide background
-                                        </div>
-                                        <div class="check-button" >
-                                          <div onclick="checkMoreOptions(this, 'hideBackground')" class="square-click left"></div>
-                                        </div>
-                                    </div>
-                                    <div class="option-check">
-                                        <div>
-                                          Hide header
-                                        </div>
-                                        <div class="check-button" >
-                                          <div onclick="checkMoreOptions(this, 'hideHeader')" class="square-click left"></div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div id="embed-selected"></div>
-                            </div>
-                        </div>
-                    <div id="alert-message"></div>
+  <div class="wrap">
+    <form id="landbot-admin-form" class="postbox">
+      <div class="form-group inside">
+        <h1>Add Landbot</h1>
+        <p>You can <a href="https://landbot.io" target="_blank">create an account here.</a></p>
+                           
+	    <div class="content-section">
+          <div>
+            <label>1. Copy and paste your landbot token here*</label>
+            <label>*You can find it under your Landbot > Share section</label>
+          </div>
+          <div class="authorization">
+            <div> TOKEN: </div>
+              <div>
+                <input
+                  class="regular-text" 
+                  name="authorization"
+                  id="authorization"
+                  placeholder="Token 7beqb8161dcbw715018i1f26axa8901b94az563b"
+                  type="text"
+                />
+              </div>
+            </div>
+          </div>
+          <div class="content-section">
+            <div>
+              <label>2. Display Format</label>
+              <label>*The way Landbot is displayed</label>
+            </div>
+            <div>
+              <div onclick="checkDisplayFormat('FULLPAGE')" class="square-display-format display-format-color">
+                <div>
+                  <img src="<?php echo plugin_dir_url( __FILE__ ) . '/assets/img/full.png'; ?>" alt="fullpage"/>
                 </div>
-
-                <hr>
-
-                <div class="inside footer">
-
-                    <button class="button button-primary" id="landbot-admin-save" type="submit">
-                        Ok
-                    </button>
-
+                <div> FULLPAGE </div>  
+              </div>
+              <div onclick="checkDisplayFormat('POPUP')" class="square-display-format display-format-color">
+                <div>
+                  <img src="<?php echo plugin_dir_url( __FILE__ ) . '/assets/img/popup.png'; ?>" alt="popup"/>
                 </div>
-            </form>
+                <div> POPUP </div>
+              </div>
+              <div onclick="checkDisplayFormat('EMBED')" class="square-display-format display-format-color">
+                <div>
+                  <img src="<?php echo plugin_dir_url( __FILE__ ) . '/assets/img/embed.png'; ?>" alt="embed"/>
+                </div>
+                <div> EMBED </div>
+              </div>
+              <div onclick="checkDisplayFormat('LIVE CHAT')" class="square-display-format display-format-color">
+                <div>
+                  <img src="<?php echo plugin_dir_url( __FILE__ ) . '/assets/img/LIVECHAT.png'; ?>" alt="LIVE CHAT"/>
+                </div>
+                <div> LIVE CHAT </div>
+              </div>
+            </div>
+          </div>
+          <div class="content-section">
+            <div>
+                <label>3. More Options</label>
+            </div>
+            <div class="more-options">
+              <div>
+                <div class="option-check">
+                  <div>
+                    Hide background
+                  </div>
+                  <div class="check-button" >
+                    <div onclick="checkMoreOptions(this, 'hideBackground')" class="square-click left"></div>
+                  </div>
+                </div>
+                <div class="option-check">
+                  <div>
+                    Hide header
+                  </div>
+                  <div class="check-button" >
+                    <div onclick="checkMoreOptions(this, 'hideHeader')" class="square-click left"></div>
+                  </div>
+                </div>
+              </div>
+              <div id="embed-selected"></div>
+            </div>
+          </div>
+          <div id="alert-message"></div>
+          </div>
+            <div class="inside footer">
+              <button class="button button-primary" id="landbot-admin-save" type="submit">
+                Ok
+              </button>
+            </div>
+      </form>
+  </div>
 
-		</div>
+  <?php
 
-		<?php
-
-	}
+  }
 }
 
 new Landbot();
